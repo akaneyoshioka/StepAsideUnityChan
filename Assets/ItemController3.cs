2939e6aa10bd732c26d0e9c59bfc4b7e7bacf931
吉岡茜 <sode_take@icloud.com>
2018-09-27 21:32:43 +0900

    StepAsideUnityChan 初めてのコミット

---
+++ Assets/ItemController3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController3 : MonoBehaviour {
+
+	public GameObject TrafficConePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnBecameInvisible(){
+		Destroy (this.gameObject);
+	}
+}
